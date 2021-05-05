e7465b78f7dbb04091c2a54e9c6703608bc13597
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-05-05 12:45:39 -0300

    fix: change site title, description, author, name and short-name

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Portfolio`,
+    description: `Juan Gabriel Pérez portfolio site v1.`,
+    author: `@JGabrielPS`,
   },
   plugins: [
     `gatsby-plugin-styled-components`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gatsby-portfolio`,
+        short_name: `portfolio`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
